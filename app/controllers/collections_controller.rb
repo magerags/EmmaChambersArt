a2728ba8e70bef204ced0c070422254708b487e5
Tom Whittle <tomwhittle@hey.com>
2021-01-04 15:44:08 +0100

    added collection actions

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,4 +1,35 @@
 class CollectionsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
 
+    def index
+      @collections = Collection.all
+    end
+
+    def show
+    end
+
+    def new
+      @collection = Collection.new
+    end
+
+    def create
+      @collection = Collection.new(collection_params)
+
+      if @collection.save
+        redirect_to collections_path
+      else
+        render :new
+      end
+    end
+
+    def destroy
+      @collection.destroy
+      redirect_to collections_path
+    end
+
+    private
+
+    def collection_params
+      params.require(:collection).permit(:name, :description)
+    end
 end
